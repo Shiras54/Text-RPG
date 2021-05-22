8877099da55a9a6e02fda9c802d2cecd4a96b2a1
Shiras54 <Hungergamespro54@gmail.com>
2021-05-22 22:45:39 +0400

    Added Player Dictionary

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hello world")
+player = {
+    'usernameInput' : input("Enter username"),
+    'passwordInput' : input("Enter password"),
+    'level' : 1,
+    'exp' : 0,
+    'strength' : 10,
+    'stamina' : 10,
+    'dexterity' : 10,
+    'intelligence' : 10,
+    'wisdom' : 10,
+    'luck' : 1,
+}
+playerDB = [
+    
+]

b7e0ab952a8bc7afbb525e7dbe5556fffcfc4b82
Shiras54 <Hungergamespro54@gmail.com>
2021-05-22 23:39:20 +0400

    made login system and limited number of attemps

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-playerDB = [
-    
-]
+game_name = str("TextRPG")
 
 
 
-class player:
-    def __init__(self, username, password):
-        self.username  = username
-        self.password = password
 
-p1 = player(Shiras54, 123)
-        
+
+
+    count = 0 
+    while True: 
+        userName = input("Hello! Welcome to " + game_name + "! \n\n Please Login  \n\nUsername: ") 
+        password = input("Password: ")
+        count += 1
+        if count == 3: 
+            print("You've run out of attempts")
+            break #exit
+        else:
+            if userName == 'elmo' and password == 'blue':
+                print("Welcome, " + userName + " You have successfully logged in")
+                break #they are in, exit loop
+            else:
+                #tell them it is wrong and have them retry, stay in loop
+                print("Incorrect username or password, Please try again. You have " + str(3-count) + " more attempts")

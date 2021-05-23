692a933961e77191f43a22b2377c59e496f946c5
Shiras54 <Hungergamespro54@gmail.com>
2021-05-23 16:42:14 +0400

    Completed Login and Register system. Added barebones Saving system

--- main.py
+++ main.py
@@ -1,22 +1,56 @@
+import json
+
 game_name = str("TextRPG")
 
-
-
-
-
-
-    count = 0 
-    while True: 
-        userName = input("Hello! Welcome to " + game_name + "! \n\n Please Login  \n\nUsername: ") 
+def register():
+    x = input("WARNING: IF YOU ALREADY HAVE A SAVE FILE, IT WILL BE DELETED TO RE-REGISTER \n ARE YOU SURE YOU WANT THIS? \n\n (y/n)")
+    if x == "y":
+        account = {
+            "id": "0",
+            "username": "",
+            "password": "",
+            "level": 1
+        }
+        username = input("Please Register  \n\nUsername: ") 
         password = input("Password: ")
-        count += 1
-        if count == 3: 
-            print("You've run out of attempts")
-            break #exit
-        else:
-            if userName == 'elmo' and password == 'blue':
-                print("Welcome, " + userName + " You have successfully logged in")
-                break #they are in, exit loop
-            else:
-                #tell them it is wrong and have them retry, stay in loop
-                print("Incorrect username or password, Please try again. You have " + str(3-count) + " more attempts")
+        
+        account["username"] = username
+        account["password"] = password
+        
+        with open("player_db.json", 'a') as player_db:
+            json.dump(account, player_db)
+        startup()
+    else: 
+        startup()
+
+def login():
+    count = 0 
+    try:
+        with open("player_db.json", 'r') as player_db:
+            playerdb = json.load(player_db)
+            while True: 
+                username = input("\n Please Login  \n\nUsername: ") 
+                password = input("Password: ")
+                count += 1
+                if count == 3: 
+                    print("You've run out of attempts")
+                    break #exit
+                else:
+                    if username == playerdb["username"] and password == playerdb["password"]:
+                        print("Welcome, " + playerdb["username"] + " You have successfully logged in")
+                        break #they are in, exit loop
+                    else:
+                        #tell them it is wrong and have them retry, stay in loop
+                        print("Incorrect username or password, Please try again. You have " + str(3-count) + " more attempts")
+    except FileNotFoundError:
+        register()
+
+
+def startup():
+    x = input("Hello! Welcome to " + game_name + "! \n (1) Login  \n (2) Register  \n")
+    if x == "1":
+        login()
+    if x == "2":
+        register()
+
+startup()
